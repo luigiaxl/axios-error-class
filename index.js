3ce642c96e73ee8c9dea7d091aaa977e6065a1cb
luigiaxl <luigi.axl.wagas@gmail.com>
2020-03-07 11:23:15 +0800

    Added option to output error logs. Improved package description.

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
 class AxiosError {
-  constructor(error) {
+  /**
+   * Initialize error instance
+   *
+   * @param error error object
+   * @param log_errors indicate whether to show logs upon initialization or not
+   */
+  constructor(error, log_errors = false) {
     if (error.response) {
+      const response = error.response
+
       // The request was made and the server responded with a status code
       // that falls out of the range of 2xx
-      this._error_data = error.response.data
-      this._error_status = error.response.status
-      this._error_headers = error.response.headers
+      this._error_data = response.data
+      this._error_status = response.status
+      this._error_headers = response.headers
+
+      if (log_errors) {
+        console.error(response.data)
+      }
     } else if (error.request) {
       // The request was made but no response was received
       // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
       // http.ClientRequest in node.js
       this._error_request = error.request
+
+      if (log_errors) {
+        console.error(error.request)
+      }
     } else {
       // Something happened in setting up the request that triggered an Error
       this._error_message = error.message
+
+      if (log_errors) {
+        console.error(error.message)
+      }
     }
 
     this._error_config = error.config
